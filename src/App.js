9423a8513a609b2b8a8f1b65a09a6cbf6139defa
Md Amir Gauhar <gauharamir15@gmail.com>
2021-07-09 18:11:22 +0530

    added bookList state and worked on UI of the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
+
+import { bookList } from "./bookList";
+
 import "./styles.css";
 
 export default function App() {
+  const [genre, setGenre] = useState(["programming", "business"]);
+  const [books, setBooks] = useState([]);
+
+  const clickHandler = (item) => {
+    setBooks(bookList[item].items);
+    // console.log(item);
+    // console.log(bookList.item);
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header>
+        <h1>📚goodreads</h1>
+      </header>
+      <div className="genre">
+        <p>select a genre to get started</p>
+        {genre.map((item, idx) => (
+          <button
+            className="genre-btn"
+            key={idx}
+            onClick={() => clickHandler(item)}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <div className="book-list">
+        {books.map((book) => (
+          <p>{book.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/bookList.js
@@ -0,0 +1,50 @@
+export const bookList = {
+  programming: {
+    id: 1,
+    title: "programming",
+    items: [
+      {
+        id: 1,
+        name: "Eloquent JS",
+        imageUrl: "https://eloquentjavascript.net/img/cover.jpg",
+        rating: 4.5
+      },
+      {
+        id: 2,
+        name: "Eloquent JS",
+        imageUrl: "https://eloquentjavascript.net/img/cover.jpg",
+        rating: 4.5
+      },
+      {
+        id: 3,
+        name: "Eloquent JS",
+        imageUrl: "https://eloquentjavascript.net/img/cover.jpg",
+        rating: 4.5
+      }
+    ]
+  },
+  business: {
+    id: 2,
+    title: "business",
+    items: [
+      {
+        id: 4,
+        name: "Rich Dad, Poor Dad",
+        imageUrl: "https://m.media-amazon.com/images/I/51AHZGhzZEL.jpg",
+        rating: 5
+      },
+      {
+        id: 5,
+        name: "Rich Dad, Poor Dad",
+        imageUrl: "https://m.media-amazon.com/images/I/51AHZGhzZEL.jpg",
+        rating: 5
+      },
+      {
+        id: 6,
+        name: "Rich Dad, Poor Dad",
+        imageUrl: "https://m.media-amazon.com/images/I/51AHZGhzZEL.jpg",
+        rating: 5
+      }
+    ]
+  }
+};
